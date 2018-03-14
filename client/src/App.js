3f62d87ee06697d5bb9ab534c418a54d93bdba17
Steven Haws <stevenjhaws@gmail.com>
2018-03-13 18:56:01 -0600

    added home, about, and error route

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About"
+import NoMatch from "./components/NoMatch"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </div>
+)
 
-export default App;
+export default App
---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const NoMatch = () => (
+  <div>
+    <h1>404 Error - Page not found</h1>
+  </div>
+)
+
+export default NoMatch
