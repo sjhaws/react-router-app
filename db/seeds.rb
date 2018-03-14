fc9b89d47dbcb02896d91999e8f38643d61e0845
Steven Haws <stevenjhaws@gmail.com>
2018-03-13 20:06:52 -0600

    added index list

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
   Product.create(
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.sentence,
-    price: Faker::Commerce.price_to_f,
+    price: Faker::Commerce.price,
     department: Faker::Commerce.department,
   )
 end
